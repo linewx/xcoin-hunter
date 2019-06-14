786a2d620887fd5c7623896abb3c4ca93977996f
ganlin.lu <ganlin.lu@zkjin.com>
2019-06-14 18:11:21 +0800

    improve stock service

--- app/runner.py
+++ app/runner.py
@@ -8,4 +8,8 @@ stock_client = StockClient(settings.STOCK.TOKEN)
 
 stock_service = StockService(db_client, stock_client)
 
-stock_service._persist_trade_cal()
+def init():
+    stock_service._persist_basic_info()
+    #stock_service._persist_trade_cal()
+
+print(stock_service.get_all_stock_info())
--- service/stock_service.py
+++ service/stock_service.py
@@ -12,9 +12,34 @@ class StockService:
         self.basic_info_table_name = 'stock_basic_info'  # 企业基础信息表
         self.trade_cal_table_name = 'stock_trade_cal'
 
+        self.trade_cal = None
+        self.stock_info = None
+        self.all_stock_info = None
+
     def _persist_trade_cal(self, start_date='', end_date=''):
         trade_cal = self.stock_client.get_trade_cal(start_date=start_date,
                                                     end_date=end_date)
-        trade_cal.to_sql(self.trade_cal_table_name, self.db_client.get_engine(), if_exists='replace')
+        trade_cal.to_sql(self.trade_cal_table_name,
+                         self.db_client.get_engine(),
+                         if_exists='replace')
+
+    def _persist_basic_info(self):
+        all_basic_info = self.stock_client.get_basic_info()
+        all_basic_info.to_sql(self.basic_info_table_name,
+                              self.db_client.get_engine(),
+                              if_exists='replace')
+
+    def get_all_stock_info(self):
+        if self.all_stock_info is None:
+            self.all_stock_info = pd.read_sql_table(self.basic_info_table_name, self.db_client.get_engine())
+        return self.all_stock_info
 
 
+    def get_all_trade_cal(self):
+        if self.trade_cal is None:
+            self.trade_cal = pd.read_sql_table(self.trade_cal_table_name, self.db_client.get_engine())
+        return self.trade_cal
+
+    def is_trade_date(self, the_date):
+        all_trade_cal = self.get_all_trade_cal()
+        return all_trade_cal.loc[all_trade_cal.cal_date == the_date]['is_open'].iloc[0]
